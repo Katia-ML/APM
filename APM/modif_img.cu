23306abb460883d6a9bab7ea15b9434eefe377d6
kamouali kamouali <kamouali@romeologin1.ro18.urca>
2023-02-24 15:36:22 +0100

    test qst 14

--- APM/modif_img.cu
+++ APM/modif_img.cu
@@ -293,13 +293,13 @@ int main (int argc , char** argv)
   dim3 block_size(32, 32);
   dim3 grid_size((width + block_size.x - 1) / block_size.x, (height + block_size.y - 1) / block_size.y);
 
-  saturate_component<<<grid_size, block_size>>>(c_d_img, width, height, 0);
-  //horizontal_flip<<<grid_size, block_size>>>(c_d_img, width, height);
+  //saturate_component<<<grid_size, block_size>>>(c_d_img, width, height, 0);
+  horizontal_flip<<<grid_size, block_size>>>(c_d_img, width, height);
   //blur<<<grid_size, block_size>>>(c_d_img, WIDTH, HEIGHT);
   //grayscale<<<grid_size, block_size>>>(c_d_img, WIDTH, HEIGHT);
   //sobel<<<grid_size, block_size>>>(c_d_img, WIDTH, HEIGHT);
-  //vertical_flip<<<grid_size, block_size>>>(c_d_img, width, height);
-  //popArt<<<grid_size, block_size>>>(c_d_img, width, height);
+  vertical_flip<<<grid_size, block_size>>>(c_d_img, width, height);
+  popArt<<<grid_size, block_size>>>(c_d_img, width, height);
 	
 
   cudaMemcpy(d_img, c_d_img, sizeof(unsigned int) * 3 * width * height, cudaMemcpyDeviceToHost);
