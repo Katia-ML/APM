9df4c8b736592a9d7845740646c2019e5b9d8ac7
kamouali kamouali <kamouali@romeologin1.ro18.urca>
2023-02-24 17:54:16 +0100

    exec modification

--- APM/modif_img.cu
+++ APM/modif_img.cu
@@ -194,7 +194,7 @@ __global__ void popArt(unsigned int *c_d_img, int width, int height)
        c_d_img[idx + 1] = 0xFF / 2;
        c_d_img[idx + 2] /= 4;
     }
-    }
+    
 
     /*  If the pixel is in the upper right quadrant of the image (y < height/2 and x > width/2 and x < width), the
     function calculates the luminance value (in grayscale) of the pixel using the formula: 0.299 * red + 0.587 * green + 0.114 * blue.
